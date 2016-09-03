fda094491eb3ea96023e579cd9c576e888e6ff1d
Ari Asulin <ari.asulin@gmail.com>
2016-09-02 23:44:27 -0700

    deploy script

--- deploy.php
+++ deploy.php
@@ -11,13 +11,20 @@ chdir('web');
 spl_autoload_extensions('.class.php');
 spl_autoload_register();
 
+// Test command
+$cmd_test = 'ssh admin.paylogicnetwork.com -t "cd /usr/share/nginx/spg; php test.php;"';
+
+// Deploy command
 $cmd_deploy = 'ssh admin.paylogicnetwork.com -t "cd /usr/share/nginx/spg; git pull;"';
 
+// Check git status
 exec('git status', $out, $ret);
 if(strpos(implode("\n", $out), 'nothing to commit, working directory clean') === false) {
     echo "Commit and push code before deploying, n00b";
     exit(1);
 }
 
+
+$ret = system($cmd_test, $out);
 
 $ret = system($cmd_deploy, $out);
