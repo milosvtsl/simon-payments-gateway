8d6ec3d0ca13315282491c237e5f2eb7c266898b
Katie Kinkel <ktkinkel@gmail.com>
2016-10-09 23:44:01 -0700

    charge fix

--- web/merchant/index.php
+++ web/merchant/index.php
@@ -19,6 +19,7 @@ spl_autoload_register();
 // Start or resume the session
 session_start();
 
+$SessionManager = new \User\Session\SessionManager();
 if(!$SessionManager->isLoggedIn()) { // !$SessionUser->hasAuthority('ROLE_ADMIN')) {
     header('Location: /login.php?message=session has ended');
     die();
