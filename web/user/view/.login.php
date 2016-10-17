368916a0a1aea56c1ebbb59a1a411029cd4ee0a6
Ari Asulin <ari.asulin@gmail.com>
2016-10-16 17:21:41 -0700

    design

--- web/user/view/.login.php
+++ web/user/view/.login.php
@@ -1,4 +1,6 @@
-<?php /** @var \User\View\LoginView $this  **/ ?>
+<?php /** @var \User\View\LoginView $this  **/
+$odd = true;
+?>
 
 
 <!-- Page Navigation -->
@@ -18,16 +20,26 @@
         <form name="form-login" class="themed" action='login.php?action=login' method='POST' id='form-login'>
             <fieldset style="display: inline-block; padding: 1em 1.5em;">
                 <legend>Sign In to your account</legend>
-                User Name:<br />
-                <input type="text" name="username" id="username"  placeholder="Username"  value="" autofocus class="themed"/>
 
-                <br />
-                <br />Password:<br />
-                <input type="password" name="password" id="password"  placeholder="Password" autocomplete="off" class="themed" />
-
-                <br />
-                <br />Login:<br />
-                <input type="submit" value="SUBMIT" class="themed" />
+                <table class="table-user-info themed">
+                    <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
+                        <td class="name">Username</td>
+                        <td>
+                            <input type="text" name="username" id="username"  placeholder="Username"  value="" autofocus class="themed"/>
+                        </td>
+                    </tr>
+                    <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
+                        <td class="name">Password</td>
+                        <td>
+                            <input type="password" name="password" id="password"  placeholder="Password" autocomplete="off" class="themed" />
+                        </td>
+                    </tr>
+                    <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
+                        <td colspan="2" style="text-align: center;">
+                            <input type="submit" value="SUBMIT" />
+                        </td>
+                    </tr>
+                </table>
             </fieldset>
         </form>
     </section>
