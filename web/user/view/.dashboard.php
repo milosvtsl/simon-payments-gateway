eb9ec4bc58393f6bc29acf3b0d7176e0a27cac05
Ari Asulin <ari.asulin@gmail.com>
2016-10-25 23:16:38 -0700

    stats

--- web/user/view/.dashboard.php
+++ web/user/view/.dashboard.php
@@ -8,9 +8,9 @@ $SessionUser = $SessionManager->getSessionUser();
 
 
 if($SessionUser->hasAuthority('ROLE_ADMIN')) {
-    $stats = OrderRow::queryStatsToDate();
+    $stats = OrderRow::queryMerchantStats();
 } else {
-    $stats = OrderRow::queryStatsToDate($SessionUser->getID());
+    $stats = OrderRow::queryMerchantStats($SessionUser->getID());
 }
 
 $button_current = 'dashboard';
@@ -33,19 +33,19 @@ include '.dashboard.nav.php';
 
         <div class="stat-box-container">
             <div class="stat-box stat-box-first">
-                <div class="stat-large">$0.00 (0)</div>
+                <div class="stat-large">$<?php echo number_format(@$stats['today'], 2); ?> (<?php echo @$stats['today_count']; ?>)</div>
                 <div>Today's Orders</div>
             </div>
             <div class="stat-box stat-box-second">
-                <div class="stat-large">$0.00 (0)</div>
+                <div class="stat-large">$<?php echo number_format(@$stats['weekly'], 2); ?> (<?php echo @$stats['weekly_count']; ?>)</div>
                 <div>This Week's Orders</div>
             </div>
             <div class="stat-box stat-box-third">
-                <div class="stat-large">$0.00 (0)</div>
+                <div class="stat-large">$<?php echo number_format(@$stats['monthly'], 2); ?> (<?php echo @$stats['monthly_count']; ?>)</div>
                 <div>This Months's Orders</div>
             </div>
             <div class="stat-box stat-box-fourth">
-                <div class="stat-large">$0.00 (0)</div>
+                <div class="stat-large">$<?php echo number_format(@$stats['yearly'], 2); ?> (<?php echo @$stats['yearly_count']; ?>)</div>
                 <div>YTD Orders</div>
             </div>
         </div>
