7109b7f81a4c70b47b5c02b6e76a4a5b2b619ad3
Ari Asulin <ari.asulin@gmail.com>
2016-10-28 21:28:39 -0700

    merchant design fixes

--- web/user/view/.dashboard.php
+++ web/user/view/.dashboard.php
@@ -26,16 +26,16 @@ if(!$stats) {
     $this->setMessage("Calculated stats in " . number_format($stats['duration'], 2) . " s");
 }
 
-$year_to_date = date('Y-01-01 00:00:00');
-$yearly  = date('Y-m-d 00:00:00', time() - 24*60*60*365 + $offset);
+$year_to_date = date('Y-01-01');
+$yearly  = date('Y-m-d', time() - 24*60*60*365 + $offset);
 
-$month_to_date = date('Y-m-01 00:00:00');
-$monthly  = date('Y-m-d 00:00:00', time() - 24*60*60*30 + $offset);
+$month_to_date = date('Y-m-01');
+$monthly  = date('Y-m-d', time() - 24*60*60*30 + $offset);
 
-$week_to_date = date('Y-m-d 00:00:00', time() - 24*60*60*date('w') + $offset);
-$weekly  = date('Y-m-d 00:00:00', time() - 24*60*60*7 + $offset);
+$week_to_date = date('Y-m-d', time() - 24*60*60*date('w') + $offset);
+$weekly  = date('Y-m-d', time() - 24*60*60*7 + $offset);
 
-$today = date('Y-m-d 00:00:00', time() - 24*60*60 + $offset);
+$today = date('Y-m-d', time() - 24*60*60 + $offset);
 
 $button_current = 'dashboard';
 include '.dashboard.nav.php';
