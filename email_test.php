e0eb8d552f926eda8593ac92482174291e02376b
Ari Asulin <ari.asulin@gmail.com>
2016-10-30 19:40:26 -0700

    pdf fix

--- email_test.php
+++ email_test.php
@@ -22,7 +22,8 @@ $SessionUser = $SessionManager->getSessionUser();
 $Order = \Order\Model\OrderRow::fetchByID(8476);
 $Merchant = \Merchant\Model\MerchantRow::fetchByID($Order->getMerchantID());
 $EmailReceipt = new \Transaction\Mail\ReceiptEmail($Order, $Merchant);
-$EmailReceipt->addAddress('ari@govpaynetwork.com');
+$EmailReceipt->addAddress('ari@govpaynetwork.com', 'Tester');
+$EmailReceipt->addBCC('ari@asu.edu', 'Tester');
 if(!$EmailReceipt->send())
     error_log("Test Email Failed: " . $EmailReceipt->ErrorInfo);
 echo "\nEmail Tests successful";
