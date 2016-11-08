d84d905a6ee201618a6a0d4b5aad2bce98abc94c
Ari Asulin <ari.asulin@gmail.com>
2016-11-07 22:01:25 -0700

    design fixes

--- web/user/view/.reset.php
+++ web/user/view/.reset.php
@@ -16,35 +16,30 @@ $this->getTheme()->renderHTMLBodyHeader(\View\Theme\AbstractViewTheme::FLAG_HEAD
             <input type="hidden" name="action" value="reset" />
             <input type="hidden" name="key" value="<?php echo $_GET['key']; ?>" />
             <input type="hidden" name="email" value="<?php echo $_GET['email']; ?>" />
-            <fieldset style="display: inline-block; padding: 0.5em; margin: 0.3em;">
+            <fieldset style="display: inline-block; padding: 0.5em; margin: 0.3em; text-align: left;">
                 <legend>RESET Password</legend>
 
                 <h3>Please enter your email address to receive a password reset link</h3>
 
                 <table class="table-user-info themed">
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Email</td>
                         <td>
                             <input type="email" name="email" id="email" disabled="disabled" value="<?php echo $_GET['email']; ?>" class="themed"/>
                         </td>
                     </tr>
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td>Change Password</td>
-                        <td><input type="password" name="password" value="" autocomplete="off" required/></td>
+                        <td><input type="password" name="password" value="" autocomplete="off" placeholder="New Password" required/></td>
                     </tr>
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td>Confirm Password</td>
-                        <td><input type="password" name="password_confirm" value="" autocomplete="off" required/></td>
+                        <td><input type="password" name="password_confirm" value="" autocomplete="off" placeholder="Confirm Password" required/></td>
                     </tr>
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Reset</td>
                         <td>
                             <input type="submit" value="SUBMIT" class="themed"/>
                         </td>
                     </tr>
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Reset</td>
-                        <td style="font-size: smaller;">
+                        <td style="text-align: center; font-weight: bold;">
                             <a href="/">Back to Login</a>
                         </td>
                     </tr>
@@ -55,27 +50,24 @@ $this->getTheme()->renderHTMLBodyHeader(\View\Theme\AbstractViewTheme::FLAG_HEAD
         <?php } else { ?>
         <form name="form-reset" class="themed" action='reset.php?action=reset' method='POST' id='form-reset'>
             <input type="hidden" name="action" value="reset" />
-            <fieldset style="display: inline-block; padding: 1em 1.5em;">
+            <fieldset style="display: inline-block; padding: 0.5em; margin: 0.3em; text-align: left;">
                 <legend>Password Reset</legend>
 
                 <span class="info">Please enter your email address to receive a password reset link</span>
 
                 <table class="table-user-info themed">
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Email</td>
                         <td>
                             <input type="email" name="email" id="email" class="themed" placeholder="Recovery Email Address" value="" autofocus required/>
                         </td>
                     </tr>
-                    <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Reset</td>
+                    <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>" style="text-align: right;">
                         <td>
                             <input type="submit" value="SUBMIT" class="themed"/>
                         </td>
                     </tr>
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Reset</td>
-                        <td style="font-size: smaller;">
+                        <td style="text-align: center; font-weight: bold;">
                             <a href="/">Back to Login</a>
                         </td>
                     </tr>
