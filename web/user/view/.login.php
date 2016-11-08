067a455990e7abd88356682425cd0d19bbd0c0ad
Ari Asulin <ari.asulin@gmail.com>
2016-11-07 21:51:07 -0700

    design fixes

--- web/user/view/.login.php
+++ web/user/view/.login.php
@@ -13,36 +13,32 @@ $this->getTheme()->renderHTMLBodyHeader(\View\Theme\AbstractViewTheme::FLAG_HEAD
     <section class="not-content login-section">
 
 
-        <form name="form-login" class="themed" action='login.php?action=login' method='POST' id='form-login' style="margin: auto;">
+        <form name="form-login" class="themed" action='login.php?action=login' method='POST' id='form-login'>
             <img src="view/theme/spg/assets/img/logo.png" alt="Simon Payments Gateway" style="display: block; margin: auto; padding: 0.5em; width: 18em;">
 
             <?php if($this->hasMessage()) echo "<h5>", $this->getMessage(), "</h5>"; ?>
 
             <fieldset style="display: inline-block; padding: 0.5em; margin: 0.3em;">
-                <legend>Login Access</legend>
+                <legend>Sign in to your account</legend>
 
                 <table class="table-user-info themed">
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Username</td>
                         <td>
                             <input type="text" name="username" id="username"  placeholder="Username"  value="" autofocus required class="themed"/>
                         </td>
                     </tr>
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Password</td>
                         <td>
                             <input type="password" name="password" id="password"  placeholder="Password" autocomplete="off" required class="themed" />
                         </td>
                     </tr>
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Login</td>
-                        <td>
-                            <input type="submit" value="SUBMIT" class="themed"/>
+                        <td style="text-align: right;">
+                            <input type="submit" value="Login" class="themed"/>
                         </td>
                     </tr>
                     <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                        <td class="name">Reset</td>
-                        <td style="font-size: smaller;">
+                        <td style="text-align: center; font-size: smaller;">
                             <a href="/reset.php">Password Reset</a>
                         </td>
                     </tr>
