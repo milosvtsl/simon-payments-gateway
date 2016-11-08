b7a4548f4458cfb0042c1d96212236c4b299a8a6
Ari Asulin <ari.asulin@gmail.com>
2016-11-07 19:10:32 -0800

    fix

--- web/order/view/.export.csv.php
+++ web/order/view/.export.csv.php
@@ -14,42 +14,45 @@ if(!$export_filename)
 header("Content-Disposition: attachment; filename=\"$export_filename\"");
 header("Content-Type: application/vnd.ms-excel");
 
-echo "Span,Count,Authorized,Settled,Void,Returned";
+echo '"Span","Count","Authorized","Settled","Void","Returned","",""';
 
 if(in_array(strtolower(@$params['action']), array('export', 'export-stats'))) {
     foreach ($ReportQuery as $Report) {
         /** @var \Order\Model\OrderQueryStats $Report */
-        echo "\n" . $Report->getGroupSpan(),
-            ',' . $Report->getCount(),
-            ',$' . $Report->getTotal(),
-            ',$' . $Report->getSettledTotal(),
-            ',$' . $Report->getVoidTotal(),
-            ',$' . $Report->getReturnTotal();
+        echo "\n\"" . $Report->getGroupSpan(),
+            '", ' . $Report->getCount(),
+            ', $' . $Report->getTotal(),
+            ', $' . $Report->getSettledTotal(),
+            ', $' . $Report->getVoidTotal(),
+            ', $' . $Report->getReturnTotal(),
+            ',,';
     }
 }
 
 if(in_array(strtolower(@$params['action']), array('export', 'export-data'))) {
-    echo "\nTotal" ,
-        ',' . $Stats->getCount(),
-        ',$' . $Stats->getTotal(),
-        ',$' . $Stats->getSettledTotal(),
-        ',$' . $Stats->getVoidTotal(),
-        ',$' . $Stats->getReturnTotal();
+    echo "\n\"Total" ,
+        '", ' . $Stats->getCount(),
+        ', $' . $Stats->getTotal(),
+        ', $' . $Stats->getSettledTotal(),
+        ', $' . $Stats->getVoidTotal(),
+        ', $' . $Stats->getReturnTotal(),
+        '';
 
     echo "\n\n";
-    echo "\nUID,Amount,Fee,Status,Mode,Date,Invoice,Cust ID,Customer";
+    echo "\nUID,Amount,Status,Mode,Date,Invoice,Cust ID,Customer";
     $offset = $SessionUser->getTimeZoneOffset();
     foreach($Query as $Order) {
         /** @var OrderRow $Order */
         echo
             "\n", $Order->getUID(),
-            ",$", $Order->getAmount(),
-            ",$", $Order->getConvenienceFee() ?: 0,
-            ",", $Order->getStatus(),
-            ",", $Order->getEntryMode(),
-            ",", $Order->getDate(),
-            ",", str_replace(',', ';', $Order->getInvoiceNumber()),
-            ",", str_replace(',', ';', $Order->getCustomerID()),
-            ",", str_replace(',', ';', $Order->getCardHolderFullName());
+            ', $', $Order->getAmount(),
+//            ", $", $Order->getConvenienceFee() ?: 0,
+            ', ', $Order->getStatus(),
+            ', ', $Order->getEntryMode(),
+            ', ', $Order->getDate(),
+            ', ', str_replace(',', ';', $Order->getInvoiceNumber()),
+            ', ', str_replace(',', ';', $Order->getCustomerID()),
+            ', ', str_replace(',', ';', $Order->getCardHolderFullName()),
+            '';
     }
 }
