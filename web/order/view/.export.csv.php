d6e80faee2dedde2726236834e58cf282913a7ec
Ari Asulin <ari.asulin@gmail.com>
2016-11-29 23:41:43 -0700

    fixes

--- web/order/view/.export.csv.php
+++ web/order/view/.export.csv.php
@@ -44,7 +44,7 @@ if(in_array(strtolower(@$params['action']), array('export', 'export-data'))) {
     foreach($Query as $Order) {
         /** @var OrderRow $Order */
         echo
-            "\n", $Order->getUID(),
+            "\n", $Order->getUID(false),
             ', $', $Order->getAmount(),
 //            ", $", $Order->getConvenienceFee() ?: 0,
             ', ', $Order->getStatus(),
--- web/order/view/.view.php
+++ web/order/view/.view.php
@@ -247,7 +247,7 @@ $Theme->printHTMLMenu('order-view', $action_url);
                         $odd = false;
                         foreach($TransactionQuery as $Transaction) { ?>
                             <tr class="row-<?php echo ($odd=!$odd)?'odd':'even';?>">
-                                <td class="hide-on-layout-vertical"><a href='/order/receipt.php?uid=<?php echo $Order->getUID(); ?>'><?php echo $Transaction->getTransactionID(); ?></a></td>
+                                <td class="hide-on-layout-vertical"><a href='/order/receipt.php?uid=<?php echo $Order->getUID(false); ?>'><?php echo $Transaction->getTransactionID(); ?></a></td>
                                 <td><?php echo date("M j g:i A", strtotime($Transaction->getTransactionDate()) + $offset); ?></td>
                                 <td>$<?php echo $Transaction->getAmount(); ?></td>
                                 <td>$<?php echo $Transaction->getServiceFee(); ?></td>
