5664af5ef2e4f1a9284d15bafdebd923012875a8
Ari Asulin <ari@govpaynetwork.com>
2016-12-03 09:33:04 -0700

    test fixes

--- web/integration/finix/test/test.php
+++ web/integration/finix/test/test.php
@@ -23,7 +23,7 @@ spl_autoload_extensions('.class.php');
 spl_autoload_register();
 
 // Register Exception Handler
-\System\Exception\ExceptionHandler::register();
+//\System\Exception\ExceptionHandler::register();
 
 
 // Test Data
