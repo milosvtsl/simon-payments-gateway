3df11988363187ad96ca587c2696b4dcdfc21ea5
Ari Asulin <ari@govpaynetwork.com>
2016-12-13 21:29:45 -0700

    login fixes

--- web/user/view/.login.php
+++ web/user/view/.login.php
@@ -14,7 +14,7 @@ $this->getTheme()->renderHTMLBodyHeader(\View\Theme\AbstractViewTheme::FLAG_HEAD
 
 
         <form name="form-login" class="themed" action='login.php?action=login' method='POST' id='form-login'>
-            <img src="view/theme/spg/assets/img/logo.png" alt="Simon Payments Gateway" style="display: block; margin: auto; padding: 0.5em; width: 18em;">
+            <img src="view/theme/spg/assets/img/logo_full.png" alt="Simon Payments Gateway" style="display: block; margin: auto; padding: 0.5em; width: 18em;">
 
             <?php if($this->hasMessage()) echo "<h5>", $this->getMessage(), "</h5>"; ?>
 
