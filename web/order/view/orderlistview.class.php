5700e25e23fe5e177721c74b1dea2abfcbd8d6e2
Ari Asulin <ari.asulin@gmail.com>
2017-02-15 22:47:27 -0700

    theme fixes

--- web/order/view/orderlistview.class.php
+++ web/order/view/orderlistview.class.php
@@ -68,21 +68,26 @@ class OrderListView extends AbstractListView {
 		}
 
         // Get Timezone diff
-        $offset = $SessionUser->getTimeZoneOffset('now');
-        $offset = 0;
+        $offset = -$SessionUser->getTimeZoneOffset('now');
+//		$offset = 0;
 
         // Set up Date conditions
+		$date_from = null;
+		$date_to = null;
 		if(!empty($params['date_from'])) {
+			$date_from = strtotime($params['date_from']);
 			$whereSQL .= "\nAND oi.date >= :from";
-			$sqlParams['from'] = date("Y-m-d G:00:00", strtotime($params['date_from']) + $offset);
-			$statsMessage .= " from " . date("M dS Y G:00", strtotime($params['date_from']) + $offset);
-			$export_filename = date("Y-m-d", strtotime($params['date_from']) + $offset) . $export_filename;
+			$sqlParams['from'] = date("Y-m-d G:00:00", $date_from + $offset);
+			$statsMessage .= " from " . date("M dS Y G:00", $date_from + $offset);
+			$export_filename = date("Y-m-d", $date_from + $offset) . $export_filename;
 		}
 		if(!empty($params['date_to'])) {
+			$date_to = strtotime($params['date_to']);
+			if($date_to < $date_from) $date_to = $date_from;
 			$whereSQL .= "\nAND oi.date <= :to";
-			$sqlParams['to'] = date("Y-m-d G:00:00", strtotime($params['date_to']) + $offset);
-			$statsMessage .= " to " . date("M dS Y G:00", strtotime($params['date_to']) + $offset);
-			$export_filename = date("Y-m-d", strtotime($params['date_to']) + $offset) . $export_filename;
+			$sqlParams['to'] = date("Y-m-d G:00:00", $date_to + $offset + 24*60*60);
+			$statsMessage .= " to " . date("M dS Y G:00", $date_to + $offset);
+			$export_filename = date("Y-m-d", $date_to + $offset) . $export_filename;
 		}
 
 
