708fc0748192a2412a85981cacba3f06b27b1e0b
Ari Asulin <ari.asulin@gmail.com>
2017-02-20 14:49:49 -0700

    timezone fixes

--- web/order/mail/receiptemail.class.php
+++ web/order/mail/receiptemail.class.php
@@ -47,7 +47,7 @@ class ReceiptEmail extends \PHPMailer
 
         $pu = parse_url(@$_SERVER['REQUEST_URI']);
         $url = (@$pu["host"]?:SiteConfig::$SITE_URL?:'localhost') . '/order/receipt.php?uid='.$Order->getUID(false);
-        $date = $Order->getDate($SessionUser->getTimeZone())->format("M dS Y g:i a");
+        $date = $Order->getDate($SessionUser->getTimeZone())->format("M dS Y g:i a e");
 
         $next_date = $Order->getSubscriptionNextDate() ? date('M dS Y G:i', strtotime($Order->getSubscriptionNextDate())) : 'N/A';
 
