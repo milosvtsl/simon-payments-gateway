9406df798482ddf9207be467418662dafc15f8bb
Ari Asulin <ari.asulin@gmail.com>
2017-02-22 16:03:46 -0700

    remove docs for master

--- scripts/db-staging-tunnel.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ssh -fNg -L 3306:localhost:3306 dbuser@dev.simonpayments.com -p 30305
--- scripts/dump-remote-db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ssh access.simonpayments.com "mysqldump paylogic" -upaylogic2 -peVw{P7mphBn
-
