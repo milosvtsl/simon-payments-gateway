3a6d0015315d3ecbd84ec8088bb24218ac8c009f
Ari Asulin <ari.asulin@gmail.com>
2017-02-28 22:49:43 -0700

    PDF REFACTOR

--- web/order/pdf/receiptpdf.class.php
+++ web/order/pdf/receiptpdf.class.php
@@ -5,137 +5,196 @@
  * Date: 8/28/2016
  * Time: 1:32 PM
  */
-namespace {
-    define('DOMPDF_DIR', dirname(dirname(__DIR__)) . '/system/lib/DOMPDF/');
+namespace Order\PDF;
+
+define('FPDF_DIR', dirname(dirname(dirname(__DIR__))) . '/support/FPDF/');
+require_once FPDF_DIR . '/fpdf.php';
 
 
-    require_once DOMPDF_DIR . '/src/Autoloader.php';
-    require_once DOMPDF_DIR . '/src/Canvas.php';
-    require_once DOMPDF_DIR . '/src/CanvasFactory.php';
-    require_once DOMPDF_DIR . '/src/Cellmap.php';
-    require_once DOMPDF_DIR . '/src/Css/AttributeTranslator.php';
-    require_once DOMPDF_DIR . '/src/Css/Color.php';
-    require_once DOMPDF_DIR . '/src/Css/Style.php';
-    require_once DOMPDF_DIR . '/src/Css/Stylesheet.php';
-    require_once DOMPDF_DIR . '/src/Dompdf.php';
-    require_once DOMPDF_DIR . '/src/Exception.php';
-    require_once DOMPDF_DIR . '/src/Exception/ImageException.php';
-    require_once DOMPDF_DIR . '/src/FontMetrics.php';
-    require_once DOMPDF_DIR . '/src/Frame/Factory.php';
-    require_once DOMPDF_DIR . '/src/Frame/FrameList.php';
-    require_once DOMPDF_DIR . '/src/Frame/FrameListIterator.php';
-    require_once DOMPDF_DIR . '/src/Frame/FrameTree.php';
-    require_once DOMPDF_DIR . '/src/Frame/FrameTreeIterator.php';
-    require_once DOMPDF_DIR . '/src/Frame/FrameTreeList.php';
-    require_once DOMPDF_DIR . '/src/Frame.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/AbstractFrameDecorator.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/Block.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/Image.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/Inline.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/ListBullet.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/ListBulletImage.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/NullFrameDecorator.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/Page.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/Table.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/TableCell.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/TableRow.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/TableRowGroup.php';
-    require_once DOMPDF_DIR . '/src/FrameDecorator/Text.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/AbstractFrameReflower.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/Block.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/Image.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/Inline.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/ListBullet.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/NullFrameReflower.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/Page.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/Table.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/TableCell.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/TableRow.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/TableRowGroup.php';
-    require_once DOMPDF_DIR . '/src/FrameReflower/Text.php';
-    require_once DOMPDF_DIR . '/src/Helpers.php';
-    require_once DOMPDF_DIR . '/src/Image/Cache.php';
-    require_once DOMPDF_DIR . '/src/JavascriptEmbedder.php';
-    require_once DOMPDF_DIR . '/src/LineBox.php';
-    require_once DOMPDF_DIR . '/src/Options.php';
-    require_once DOMPDF_DIR . '/src/PhpEvaluator.php';
-    require_once DOMPDF_DIR . '/src/Positioner/AbstractPositioner.php';
-    require_once DOMPDF_DIR . '/src/Positioner/Absolute.php';
-    require_once DOMPDF_DIR . '/src/Positioner/Block.php';
-    require_once DOMPDF_DIR . '/src/Positioner/Fixed.php';
-    require_once DOMPDF_DIR . '/src/Positioner/Inline.php';
-    require_once DOMPDF_DIR . '/src/Positioner/ListBullet.php';
-    require_once DOMPDF_DIR . '/src/Positioner/NullPositioner.php';
-    require_once DOMPDF_DIR . '/src/Positioner/TableCell.php';
-    require_once DOMPDF_DIR . '/src/Positioner/TableRow.php';
-    require_once DOMPDF_DIR . '/src/Renderer/AbstractRenderer.php';
-    require_once DOMPDF_DIR . '/src/Renderer/Block.php';
-    require_once DOMPDF_DIR . '/src/Renderer/Image.php';
-    require_once DOMPDF_DIR . '/src/Renderer/Inline.php';
-    require_once DOMPDF_DIR . '/src/Renderer/ListBullet.php';
-    require_once DOMPDF_DIR . '/src/Renderer/TableCell.php';
-    require_once DOMPDF_DIR . '/src/Renderer/TableRowGroup.php';
-    require_once DOMPDF_DIR . '/src/Renderer/Text.php';
-    require_once DOMPDF_DIR . '/src/Renderer.php';
-    require_once DOMPDF_DIR . '/src/Adapter/GD.php';
-    require_once DOMPDF_DIR . '/lib/Cpdf.php';
-    require_once DOMPDF_DIR . '/src/Adapter/CPDF.php';
-    require_once DOMPDF_DIR . '/src/Adapter/PDFLib.php';
-
-//    require_once DOMPDF_DIR . '/lib/html5lib/Parser.php';
-//    require_once DOMPDF_DIR . '/lib/php-font-lib/src/FontLib/Autoloader.php';
-//    require_once DOMPDF_DIR . '/lib/php-svg-lib/src/autoload.php';
-//
-//    require_once DOMPDF_DIR . '/src/Autoloader.php';
+use Merchant\Model\MerchantRow;
+use Order\Model\OrderRow;
+use Order\Model\TransactionRow;
+use Order\View\OrderView;
+use System\Config\DBConfig;
+use System\Config\SiteConfig;
+use User\Session\SessionManager;
+use View\Theme\Blank\BlankViewTheme;
 
 
-//    require_once DOMPDF_DIR . '/autoload.inc.php';
+class ReceiptPDF extends \FPDF
+{
+    private $order, $merchant;
+    public function __construct(OrderRow $Order, MerchantRow $Merchant) {
+        parent::__construct();
 
-//    spl_autoload_register('\Dompdf\dompdf_autoload', true, true);
-//
-//
-//    function dompdf_autoload($class) {
-//        if (0 === strncmp('Cpdf', $class, 4)) {
-//            $file = DOMPDF_DIR . 'lib/Cpdf.php';
-//            require_once $file;
-//        }
-//        if (0 === strncmp('Dompdf', $class, 6)) {
-//            $file = DOMPDF_DIR . 'src/' . str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 6)) . '.php';
-//            require_once $file;
-//        }
-//    }
-}
+        $this->order = $Order;
+        $this->merchant = $Merchant;
 
-namespace Order\PDF {
+        $SessionManager = new SessionManager();
+        $SessionUser = $SessionManager->getSessionUser();
 
-    use Dompdf\Dompdf;
-    use Merchant\Model\MerchantRow;
-    use Order\Model\OrderRow;
-    use Order\View\OrderView;
-    use View\Theme\Blank\BlankViewTheme;
+        $OrderView = new OrderView($Order->getID(), 'download');
+        $OrderView->setTheme(new BlankViewTheme());
 
 
-    class ReceiptPDF extends Dompdf
-    {
-        public function __construct(OrderRow $Order, MerchantRow $Merchant) {
-            parent::__construct();
+        $this->AliasNbPages();
+        $this->AddPage();
 
-            $OrderView = new OrderView($Order->getID(), 'download');
-            $OrderView->setTheme(new BlankViewTheme());
+        switch(strtolower($Order->getEntryMode())) {
+            case 'keyed':
+            case 'swipe':
+                $this->addCreditCardInfo();
+                break;
 
-            ob_start();
-
-            $OrderView->renderHTML();
-
-            $output = ob_get_contents();
-
-            ob_end_clean();
-            $this->loadHtml($output);
-
-            //        $this->setPaper('A4', 'landscape');
+            case 'check':
+                $this->addCheckInfo();
         }
+
+        // Line break
+        $this->Ln(4);
+
+        $this->SetFont('Courier','B',16);
+        $this->Cell(0,10,SiteConfig::$SITE_DEFAULT_MERCHANT_NAME . ': ' . $Merchant->getName(),0,1);
+
+        $this->SetFont('Courier','',12);
+
+        $this->Cell(0,6,'Address:      ' . $Merchant->getAddress() . $Merchant->getAddress2(),0,1);
+        $this->Cell(0,6,'City:         ' . $Merchant->getCity(),0,1);
+        $this->Cell(0,6,'State:        ' . $Merchant->getState(),0,1);
+        $this->Cell(0,6,'Zip:          ' . $Merchant->getZipCode(),0,1);
+        $this->Cell(0,6,'Phone:        ' . $Merchant->getTelephone(),0,1);
+
+
+        $this->Cell(0,6,'Date:         ' . $Order->getDate($SessionUser->getTimeZone())->format("F jS Y"),0,1);
+        $this->Cell(0,6,'Time:         ' . $Order->getDate($SessionUser->getTimeZone())->format("g:i:s A T"),0,1);
+
+
+        // Line break
+        $this->Ln(4);
+
+        $this->SetFont('Courier','B',16);
+        $this->Cell(0,10,"Total:    $" . $Order->getAmount(),0,1);
+
+        $this->SetFont('Courier', '', 12);
+        if($Order->getConvenienceFee()) {
+            $this->Cell(0, 6, 'Conv. Fee:    ' . $Order->getConvenienceFee(), 0, 1);
+            $this->Cell(0, 6, 'Subtotal:     ' . number_format($Order->getAmount()+$Order->getConvenienceFee(), 2), 0, 1);
+        }
+
+        if($Order->getTotalReturnedAmount()>0) {
+            $this->Cell(0, 6, 'Returned:     ' . number_format($Order->getTotalReturnedAmount(), 2), 0, 1);
+        }
+
+        // Line break
+        $this->Ln(6);
+
+        /** @var \Order\Model\TransactionRow $Transaction */
+        $DB = DBConfig::getInstance();
+        $TransactionQuery = $DB->prepare(TransactionRow::SQL_SELECT . "WHERE t.order_item_id = ? LIMIT 100");
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        $TransactionQuery->setFetchMode(\PDO::FETCH_CLASS, TransactionRow::_CLASS);
+        $TransactionQuery->execute(array($Order->getID()));
+
+
+        $this->SetFont('Courier', 'B', 10);
+        $this->Cell(0, 6,
+            sprintf("%-' 24s %-' 24s %-' 24s %-' 24s ", "TID", "Date", "Amount", "Action")
+            , 1, 1);
+
+
+        $this->SetFont('Courier', '', 10);
+        foreach($TransactionQuery as $Transaction) {
+            $this->Cell(0, 6,
+                sprintf("%-' 24s %-' 24s %-' 24s %-' 24s ",
+                    $Transaction->getIntegrationRemoteID(),
+                    $Transaction->getTransactionDate($SessionUser->getTimeZone())->format("M j g:i A"),
+                    $Transaction->getAmount(),
+                    $Transaction->getAction())
+                , 0, 1);
+        }
+
+
+    }
+
+    private function addCreditCardInfo() {
+        $Order = $this->order;
+        $this->SetFont('Courier','B',16);
+        $this->Cell(0,10,SiteConfig::$SITE_DEFAULT_CUSTOMER_NAME . ': ' . $Order->getCustomerFullName(),0,1);
+
+        $this->SetFont('Courier','',12);
+        $this->Cell(0,6,'Credit Card   ' . $Order->getCardNumber(),0,1);
+        $this->Cell(0,6,'Card Type     ' . $Order->getCardType(),0,1);
+        $this->Cell(0,6,'Card Exp      ' . $Order->getCardExpMonth(). '/'. $Order->getCardExpYear(),0,1);
+        if($Order->getInvoiceNumber())
+            $this->Cell(0,6,'Invoice       ' . $Order->getInvoiceNumber(),0,1);
+
+
+    }
+
+    private function addCheckInfo() {
+
     }
 
 
+    public function render($dest='', $name='', $isUTF8=false) {
+        if($name)
+            $this->SetTitle($name);
+        $this->Output($dest, $name, $isUTF8);
+    }
+
+
+    // Page header
+    function Header()
+    {
+        $Merchant = $this->merchant;
+        $Order = $this->order;
+        $webDir = dirname(dirname(__DIR__));
+        $pu = parse_url(@$_SERVER['REQUEST_URI']);
+        $url = (@$pu["host"]?:SiteConfig::$SITE_URL?:'localhost') . '/order/receipt.php?uid='.$Order->getUID();
+
+        if($Merchant->hasLogoPath()) {
+            // Logo
+            $this->Image($webDir . '/' . $Merchant->getLogoImageURL(),10,6,60, NULL, NULL, $url);
+        }
+
+        $this->Line(10, 32, 200, 32);
+
+        // Move to the right
+//        $this->Cell(130);
+
+        $SessionManager = new SessionManager();
+        $SessionUser = $SessionManager->getSessionUser();
+        $date = $Order->getDate($SessionUser->getTimeZone())->format("M dS Y");
+        $time = $Order->getDate($SessionUser->getTimeZone())->format("g:i A T");
+
+        $TEXT = <<<TEXT
+{$Merchant->getShortName()}
+{$date}
+{$time}
+TEXT;
+
+
+        // Title
+        $this->SetFont('Courier','',12);
+//        $this->Cell(190,0,  $Merchant->getShortName(),0,1,'R');
+        $this->MultiCell(190,5, $TEXT,0,'R');
+        $this->SetTextColor(0, 0, 255);
+        $this->Cell(190,5, $Order->getReferenceNumber(),0,1,'R', false, $url);
+
+        // Line break
+        $this->Ln(6);
+    }
+
+    // Page footer
+    function Footer()
+    {
+        // Position at 1.5 cm from bottom
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial','I',8);
+        // Page number
+//        $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    }
 }
+
 
